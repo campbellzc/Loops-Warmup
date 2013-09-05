147e6fdd482a23c9558e6c0dcb8db8104b72c3b3
Campbell <campbellzc@s.dcsdk12.org>
2013-09-05 08:33:25 -0600

    first exercise complete

--- loopswarmup.java
+++ loopswarmup.java
@@ -1,5 +1,17 @@
 public class loopswarmup {
 	public static void main(String[] args) {
+		warmUp0(5);
+	}
+	public static void warmUp0(int n) {
 		
+		int total = 0;
+		while (n >= 1) {
+			System.out.println(n);
+			total = total + n;
+			n = n-1;
+			
+
+		}
+		System.out.println(total);
 	}
 }
